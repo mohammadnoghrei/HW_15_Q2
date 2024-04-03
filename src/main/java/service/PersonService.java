23a243f7498fce4605bdb2376624dc28f9b5d318
mohammadnoghrei <noghreimohammad@gmail.com>
2024-04-03 21:45:26 +0330

    create personSer

---
+++ src/main/java/service/PersonService.java
@@ -0,0 +1,29 @@
+package service;
+
+import model.Person;
+import repository.PersonRepository;
+
+import java.util.List;
+
+public class PersonService  {
+    private final PersonRepository personRepository=new PersonRepository();
+    public void save(Person person){
+        personRepository.save(person);
+    }
+    public void update(Person person){
+        personRepository.update(person);
+    }
+
+    public void remove(long id){
+        Person person =personRepository.findById(id);
+        personRepository.removePerson(person);
+    }
+//    public void removePerson(Person person){
+//        personRepository.removePerson(person);
+//    }
+    public List<Person> findAll(){
+        return personRepository.findAll();
+    }
+    public void contains(){}
+
+}
