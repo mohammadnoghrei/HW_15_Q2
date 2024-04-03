fb87621da0661a7a70f25bb40ede465e05984552
mohammadnoghrei <noghreimohammad@gmail.com>
2024-04-03 22:19:35 +0330

    fix method of teacher

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -1,16 +1,32 @@
 package model;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Teacher extends Person {
     long teacherCode;
     int salary;
     String  diploma;
+    MASTER_DEGREE masterDegree;
 
 
 
+    public Teacher(String firstName, String lastName, Date birthDate, long teacherCode, int salary, String diploma, MASTER_DEGREE masterDegree) {
+        super(firstName, lastName, birthDate);
+        this.teacherCode = teacherCode;
+        this.salary = salary;
+        this.diploma = diploma;
+        this.masterDegree = masterDegree;
+    }
 }
-enum MASTER_DEGREE{
 
-}
