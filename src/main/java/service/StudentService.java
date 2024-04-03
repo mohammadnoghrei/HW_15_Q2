8a26f1c936fbff3a389bd800cd9e6f23883f6c3f
mohammadnoghrei <noghreimohammad@gmail.com>
2024-04-03 21:46:48 +0330

    create studentser

---
+++ src/main/java/service/StudentService.java
@@ -0,0 +1,31 @@
+package service;
+
+import model.Person;
+import model.Student;
+import repository.PersonRepository;
+import repository.StudentRepository;
+
+import java.util.List;
+
+public class StudentService {
+
+    private final StudentRepository studentRepository=new StudentRepository();
+    public void save(Student student){
+        studentRepository.save(student);
+    }
+    public void update(Student student){
+        studentRepository.update(student);
+    }
+
+    public void remove(long id){
+        Student student =studentRepository.findById(id);
+        studentRepository.removePerson(student);
+    }
+    //    public void removePerson(Person person){
+//        personRepository.removePerson(person);
+//    }
+    public List<Student> findAll(){
+        return studentRepository.findAll();
+    }
+    public void contains(){}
+}
